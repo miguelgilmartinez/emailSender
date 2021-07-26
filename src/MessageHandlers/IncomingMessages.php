e66252a4fae6545076cafbf210f06375a1ee2ed0
MiguelGil <miguelgilmartinez@gmail.com>
2021-07-26 19:03:12 +0200

    Adding Message broker client

---
+++ src/MessageHandlers/IncomingMessages.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\MessageHandlers;
+
+use App\Messages\MyMessage;
+
+class IncomingMessages
+{
+    public function __invoke(MyMessage $message)
+    {
+        
+    }
+}
---
+++ src/Messages/MyMessage.php
@@ -0,0 +1,5 @@
+<?php 
+namespace App\Messages;
+class MyMessage {
+
+}
